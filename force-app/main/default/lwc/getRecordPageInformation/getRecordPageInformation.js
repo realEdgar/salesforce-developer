e0b7f24a2607dac0d9e2f669eec872b885b6139a
Edgar Lopez <edgarlopez@MacBook-Air-de-Edgar.local>
2025-06-02 17:26:24 -0600

    Account Layout Sections using getRecordUi method from 'lightning/uiRecordApi'

--- force-app/main/default/lwc/getRecordPageInformation/getRecordPageInformation.js
+++ force-app/main/default/lwc/getRecordPageInformation/getRecordPageInformation.js
@@ -1,11 +1,12 @@
 import { LightningElement, api, wire } from 'lwc';
-import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import { getRecord, getFieldValue, getRecordUi } from 'lightning/uiRecordApi';
 import ACCOUNT_NAME from '@salesforce/schema/Account.Name';
 
 const fields = [ ACCOUNT_NAME ]
 
 export default class GetRecordPageInformation extends LightningElement {
     @api recordId;
+    accountLayoutSections = [];
 
     @wire(getRecord, {
         recordId: '$recordId',
@@ -14,5 +15,14 @@ export default class GetRecordPageInformation extends LightningElement {
 
     get recordName(){
         return getFieldValue(this.record.data, ACCOUNT_NAME);
+    }
+
+    @wire(getRecordUi, { recordIds: '$recordId', layoutTypes: ['Full'], modes: ['View'] })
+    handleRecordLayout({ error, data }){
+        if(data){
+            this.accountLayoutSections = data.layouts.Account["012000000000000AAA"].Full.View.sections;
+        } else if(error){
+            console.error(error);
+        }
     }
 }
