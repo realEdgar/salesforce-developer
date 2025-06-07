75d11b8beb6c4b2178ca5023df02d34ea0e4efd3
Edgar L <edg.lop.arr@gmail.com>
2025-06-07 09:14:59 -0600

    Project: Get Object Info - Build object selection functionality LWC + Apex

--- force-app/main/default/lwc/getObjectInformation/getObjectInformation.js
+++ force-app/main/default/lwc/getObjectInformation/getObjectInformation.js
@@ -1,3 +1,31 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getAvailableObjects from '@salesforce/apex/GetObjectInfoController.fetchAvailableObjectsInTheOrg';
 
-export default class GetObjectInformation extends LightningElement {}
+export default class GetObjectInformation extends LightningElement {
+    selectedObject;
+    availableObjectOptions = [];
+    isLoading = true;
+    
+    @wire(getAvailableObjects)
+    handleObjectsAvailable({ error, data }){
+        if(data){
+            this.availableObjectOptions = JSON.parse(data).map(option => {
+                return {
+                    label: option.label,
+                    value: option.objectApiName
+                }
+            }).sort((a, b) => {
+                if(a.label > b.label) return 1;
+                else return -1;
+            });
+            this.isLoading = false;
+        } else if(error) {
+            console.error(error);
+            this.isLoading = false;
+        }
+    }
+
+    handleObjectSelection(e){
+        this.selectedObject = e.detail.value;
+    }
+}
