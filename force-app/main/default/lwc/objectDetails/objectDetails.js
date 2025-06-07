fa7ec6e65613d71000aa38dcb45b75dec52f71a6
Edgar L <edg.lop.arr@gmail.com>
2025-06-07 11:03:47 -0600

    Project: Get Object Info - Building Child Component (initial setup) to show information related to the object selected. additionally add header and information related to the child related objects.

---
+++ force-app/main/default/lwc/objectDetails/objectDetails.js
@@ -0,0 +1,16 @@
+import { LightningElement, api } from 'lwc';
+
+const COLS_CHILDRELATED = [
+    { label: 'Child Object', fieldName: 'childObjectApiName', type: 'text' },
+    { label: 'Field Api Name', fieldName: 'fieldName', type: 'text' },
+    { label: 'Relationship Name', fieldName: 'relationshipName', type: 'text' },
+];
+
+export default class ObjectDetails extends LightningElement {
+    @api objectDetails;
+    childObjectsColumns = COLS_CHILDRELATED;
+
+    get theme(){
+        return `background-color: #${this.objectDetails.themeInfo.color}`;
+    }
+}
