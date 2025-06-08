3a29bc6c9c585a034f43ce36e3270ddd6cf309d6
Edgar L <edg.lop.arr@gmail.com>
2025-06-08 09:33:43 -0600

    Project: Get Object Info - Adding Fields Object to the objectDetails - Adding UI logic when data related to child object does not retrieve data.

--- force-app/main/default/lwc/objectDetails/objectDetails.js
+++ force-app/main/default/lwc/objectDetails/objectDetails.js
@@ -5,12 +5,28 @@ const COLS_CHILDRELATED = [
     { label: 'Field Api Name', fieldName: 'fieldName', type: 'text' },
     { label: 'Relationship Name', fieldName: 'relationshipName', type: 'text' },
 ];
+const COLS_FIELDS = [
+    { label: 'Field Label', fieldName: 'label', type: 'text' },
+    { label: 'Field Api Name', fieldName: 'apiName', type: 'text' },
+    { label: 'Field Type', fieldName: 'dataType', type: 'text' },
+];
 
 export default class ObjectDetails extends LightningElement {
     @api objectDetails;
     childObjectsColumns = COLS_CHILDRELATED;
+    objectFieldColumns = COLS_FIELDS;
 
     get theme(){
         return `background-color: #${this.objectDetails.themeInfo?.color ?? 'fff'}`;
+    }
+    
+    get fieldsData(){
+        let data = [];
+        for(let fieldInfo in this.objectDetails.fields){
+            data.push(
+                this.objectDetails.fields[fieldInfo]
+            );
+        }
+        return data;        
     }
 }
